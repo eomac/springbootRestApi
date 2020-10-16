e16dbcac0291df2eb3a542d22cf7be73ccddd5c5
jacace <javierandrescaceres@gmail.com>
2020-10-16 17:16:47 -0500

    test new user

--- src/test/java/com/example/demo/HomeControllerTest.java
+++ src/test/java/com/example/demo/HomeControllerTest.java
@@ -24,6 +24,6 @@ public class HomeControllerTest {
     public void getHello() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("  Spring Boot!")));
+                .andExpect(content().string(equalTo("   Boot!")));
     }
 }
